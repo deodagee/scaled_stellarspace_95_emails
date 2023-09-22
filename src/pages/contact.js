1fb20fe7cca7cb3631f3abe649a1c919e8823d68
unknown <deo_singiza@hotmail.com>
2023-09-22 17:41:44 -0400

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ function Home() {
         <>
             <Header
             ></Header>
-                <Contact></Contact>
+            <Contact></Contact>
             <Footer></Footer>
         </>
     )
