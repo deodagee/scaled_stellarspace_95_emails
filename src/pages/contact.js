ea5f7cd628e6279878ff8496d10ba35c65380b11
unknown <deo_singiza@hotmail.com>
2023-09-21 21:02:22 -0400

    first commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,27 +1,15 @@
 import Header from './components/header'
 import Contact from './components/contact'
 import Footer from "./components/footer"
-import styles from "../styles/components/contact.module.css"
 
 
 function Home() {
     return (
         <>
-
             <Header
             ></Header>
-
-            <div className={styles.contact_parent}>
                 <Contact></Contact>
-            </div>
-
-
-<div className={styles.contact_footer}>
-
             <Footer></Footer>
-            </div>
-
-
         </>
     )
 }
