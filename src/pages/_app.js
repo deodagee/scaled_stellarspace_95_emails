5eff2ddc51ea4799efb635f653b73fbc3b607e93
unknown <deo_singiza@hotmail.com>
2023-09-23 04:56:41 -0400

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,32 @@
-import { ChakraProvider, StylesProvider } from "@chakra-ui/react" ;
+// pages/_app.js
+
+import React from 'react';
+import App from 'next/app';
+import Head from 'next/head';
+import { ChakraProvider } from "@chakra-ui/react";
 import Fonts from "./components/fonts";
 import { AnimatePresence } from "framer-motion";
-import { SessionProvider } from "next-auth/react"
+import { SessionProvider } from "next-auth/react";
 
-const Website = ({Component, pageProps, router, session}) => {
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, router, session } = this.props;
     return (
+      <>
+        <Head>
+          <link rel="stylesheet" href="/path/to/your.css" />
+        </Head>
         <SessionProvider session={session}>
-    <ChakraProvider>
-        <Fonts />
-        <AnimatePresence mode='sync' initial={true}>
-            <Component {...pageProps} key={router.route} /> 
-        </AnimatePresence>
-    </ChakraProvider>
-    </SessionProvider>
-    )
+          <ChakraProvider>
+            <Fonts />
+            <AnimatePresence mode='sync' initial={true}>
+              <Component {...pageProps} key={router.route} /> 
+            </AnimatePresence>
+          </ChakraProvider>
+        </SessionProvider>
+      </>
+    );
+  }
 }
 
-export default Website
+export default MyApp;
