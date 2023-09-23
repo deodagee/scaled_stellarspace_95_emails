4603976121acd3e79fe824159f59f47c1fb6be2c
unknown <deo_singiza@hotmail.com>
2023-09-23 12:58:53 -0400

    first commit

--- src/pages/components/body.js
+++ src/pages/components/body.js
@@ -295,81 +295,80 @@ function Body() {
             </div>
 
 
-            <ol className={styles.top_main_header_and_paragraphs} >
-              <ul className={styles.top_main_header}>
-                <li>
-                  <h1 className={styles.top_main_header_header1}>Stellar Space Plans</h1>
-                </li>
-                <li>
-                  <h2 className={styles.top_main_header_header2}>Deals That Make Sense</h2>
-                </li>
-                <li>
-                  <Link className={styles.contact_page_link} href={'/contact'}><h2 className={styles.top_main_header_button}>Contact</h2></Link>
-                </li>
-              </ul>
-              <ul className={styles.top_main_paragraphs}>
-                <li>
-                  <Image
-                    className={styles.top_main_icons_target}
-                    width={50}
-                    height={50}
-                    alt=""
-                    src="/target.png"
-                    onLoad={handleImageLoad}
-                  >
-                  </Image>
-                  <p className={styles.top_main_header_paragraph1}>Picking and Choosing</p>
-                  <p className={styles.top_main_header_paragraph2}>For Individuals and Businesses alike, if you need help picking a plan, send a request through the <Link className={styles.contact_page_link} href={"/contact"}>Contact</Link> page. Otherwise choose a plan, and click on &quot;Get Started&quot; to send me an order. This is not a payment gate; please allow  1-2 business days for a reply first, so we can get started.</p>
-                </li>
-                <li>
-                  <Image
-                    className={styles.top_main_icons_chip}
-                    width={50}
-                    height={50}
-                    alt=""
-                    src="/chip.png"
-                    onLoad={handleImageLoad}
-                  >
-                  </Image>
-                  <p className={styles.top_main_header_paragraph1}>Payments</p>
-                  <p className={styles.top_main_header_paragraph2}>You don&apos;t pay anything until we&apos;ve got your plans sorted out. This means that until you have the rough design in your hands, you don&apos;t have to worry about spending a dime. Click <button onClick={togglePopup}> <p className={styles.contact_page_link}> Rough Designs</p></button> for more information.</p>
+            <div className={styles.container}>
+  <ol className={styles.top_main_header_and_paragraphs}>
+    <ul className={styles.top_main_header}>
+      <li>
+        <h1 className={styles.top_main_header_header1}>Stellar Space Plans</h1>
+      </li>
+      <li>
+        <h2 className={styles.top_main_header_header2}>Deals That Make Sense</h2>
+      </li>
+      <li>
+        <Link className={styles.contact_page_link} href={'/contact'}><h2 className={styles.top_main_header_button}>Contact</h2></Link>
+      </li>
+    </ul>
+    <ul className={styles.top_main_paragraphs}>
+      <li>
+        <Image
+          className={styles.top_main_icons_target}
+          width={50}
+          height={50}
+          alt=""
+          src="/target.png"
+          onLoad={handleImageLoad}
+        />
+        <p className={styles.top_main_header_paragraph1}>Picking and Choosing</p>
+        <p className={styles.top_main_header_paragraph2}>For Individuals and Businesses alike, if you need help picking a plan, send a request through the <Link className={styles.contact_page_link} href={"/contact"}>Contact</Link> page. Otherwise choose a plan, and click on &quot;Get Started&quot; to send me an order. This is not a payment gate; please allow  1-2 business days for a reply first, so we can get started.</p>
+      </li>
+      <li>
+        <Image
+          className={styles.top_main_icons_chip}
+          width={50}
+          height={50}
+          alt=""
+          src="/chip.png"
+          onLoad={handleImageLoad}
+        />
+        <p className={styles.top_main_header_paragraph1}>Payments</p>
+        <p className={styles.top_main_header_paragraph2}>You don&apos;t pay anything until we&apos;ve got your plans sorted out. This means that until you have the rough design in your hands, you don&apos;t have to worry about spending a dime. Click <button onClick={togglePopup}> <p className={styles.contact_page_link}> Rough Designs</p></button> for more information.</p>
 
-                  <p id="popup" className={styles.popup}>
-                    <p className={styles.popupContent}>
-                      Rough Design: It consists of a visual blueprint, depicting the basic elements, design and layout of the project. With stellarspace, your rough design comes free of charge. You may request html, javascript , css, png, pdf, doc, .blend , .wav, mp3, type files and more. Please alow up to 3 business days for a rough design to be send out back to you.
-                    </p>
-                    <button onClick={togglePopup}> <p className={styles.button_toggle_close_paragraph}>Close </p></button>
-                  </p>
-                </li>
-                <li>
-                  <Image
-                    className={styles.top_main_icons_mountain}
-                    width={50}
-                    height={50}
-                    alt=""
-                    src="/mountain.png"
-                    onLoad={handleImageLoad}
-                  >
-                  </Image>
-                  <p className={styles.top_main_header_paragraph1}>Premium plans</p>
-                  <p className={styles.top_main_header_paragraph2}> <Link className={styles.contact_page_link} href={"/eplus"}> Premium </Link> plans for those that need more. Cover yourself with at least 8GB of ram space.</p>
-                </li>
-                <li>
-                  <Image
-                    className={styles.top_main_icons_features}
-                    width={50}
-                    height={50}
-                    alt=""
-                    src="/gears.png"
-                    onLoad={handleImageLoad}
-                  >
-                  </Image>
-                  <p className={styles.top_main_header_paragraph1}>Features</p>
-                  <p className={styles.top_main_header_paragraph2}>Get all the features listed, and more. Please send any special requests through the <Link className={styles.contact_page_link} href={"/contact"}>contact</Link> page. Thank You kindly.</p>
-                </li>
-              </ul>
+        {/* Popup */}
+        <div id="popup" className={styles.popup}>
+          <div className={styles.popupContent}>
+            Rough Design: It consists of a visual blueprint, depicting the basic elements, design and layout of the project. With stellarspace, your rough design comes free of charge. You may request html, javascript , css, png, pdf, doc, .blend , .wav, mp3, type files and more. Please alow up to 3 business days for a rough design to be send out back to you.
+          </div>
+          <button onClick={togglePopup}> <p className={styles.button_toggle_close_paragraph}>Close </p></button>
+        </div>
+      </li>
+      <li>
+        <Image
+          className={styles.top_main_icons_mountain}
+          width={50}
+          height={50}
+          alt=""
+          src="/mountain.png"
+          onLoad={handleImageLoad}
+        />
+        <p className={styles.top_main_header_paragraph1}>Premium plans</p>
+        <p className={styles.top_main_header_paragraph2}> <Link className={styles.contact_page_link} href={"/eplus"}> Premium </Link> plans for those that need more. Cover yourself with at least 8GB of ram space.</p>
+      </li>
+      <li>
+        <Image
+          className={styles.top_main_icons_features}
+          width={50}
+          height={50}
+          alt=""
+          src="/gears.png"
+          onLoad={handleImageLoad}
+        />
+        <p className={styles.top_main_header_paragraph1}>Features</p>
+        <p className={styles.top_main_header_paragraph2}>Get all the features listed, and more. Please send any special requests through the <Link className={styles.contact_page_link} href={"/contact"}>contact</Link> page. Thank You kindly.</p>
+      </li>
+    </ul>
+  </ol>
+</div>
 
-            </ol>
 
 
             <ul className={styles.essentials_list}>
