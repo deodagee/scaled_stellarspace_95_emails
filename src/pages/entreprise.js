230afbe07cb6c099a5354573f8d6e22927373177
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-09-28 00:46:31 -0400

    Add files via upload

---
+++ src/pages/entreprise.js
@@ -0,0 +1,17 @@
+import Header from './components/header'
+import Entreprise from "./components/entreprise"
+import Footer from "./components/footer"
+
+
+function EntreprisePage() {
+  return (
+    <>
+      <Header></Header>
+      <Entreprise></Entreprise>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default EntreprisePage;
+
