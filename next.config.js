ad30446f208544c0d02a69ae90d0eb9529cc3270
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-09-29 19:48:50 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Your Next.js configuration options here
-  };
-  
-  module.exports = nextConfig;
-  
+  // Your Next.js configuration options here
+  webpack: (config, { isServer }) => {
+    // Enable source maps for better debugging in development and production
+    if (!isServer) {
+      config.devtool = 'source-map';
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
