a6dce6abf0d4b6157c188b27dc1da4f9552320be
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-05 19:36:23 -0400

    Add files via upload

---
+++ src/pages/info/privatedomains.js
@@ -0,0 +1,29 @@
+import { Inter } from 'next/font/google'
+import Header from '../components/header'
+import PrivateDomains from "../components/info/privatedomains"
+import Footer from "../components/footer"
+import styles from "../../styles/components/info/privatedomains.module.css"
+const inter = Inter({ subsets: ['latin'] })
+
+function PrivateD() {
+  return (
+    <>
+    <div className={styles.front_end_apis_page}>
+    <div className={styles.front_end_apis_page_wrapper}>
+    <div className={styles.front_end_apis_page_header}>
+      <Header></Header>
+      </div>
+      <div className={styles.front_end_apis_page_body}>
+      <PrivateDomains></PrivateDomains>
+      </div>
+      <div className={styles.front_end_apis_page_footer}>
+      <Footer></Footer>
+      </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default PrivateD;
+
