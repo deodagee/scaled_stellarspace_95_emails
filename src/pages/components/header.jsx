a071e5ef052dc7d6f5b447a96269dd06c8205887
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-09 20:34:40 -0400

    Add files via upload

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,347 +1,283 @@
-@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap');
-/*font-family: 'Oswald', sans-serif;*/
-
-@import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300&display=swap');
-
-
-@import url('https://fonts.googleapis.com/css2?family=Cabin:ital@1&display=swap');
-/*  font-family: 'Cabin', sans-serif;
-*/
-
-@import url('https://fonts.googleapis.com/css2?family=Oxygen:wght@300;400;700&display=swap');
+import styles from "../../styles/components/header.module.css";
+import xsmStyles from "../../styles/components/header_xsm_mobile.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import { useSession, signIn, signOut } from "next-auth/react"
+import React, { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Loading from './loading';
 
 
-@import url('https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap');
-/*  font-family: 'Teko', sans-serif;
-*/
+function Header() {
+  const [isCssLoaded, setIsCssLoaded] = useState(false);
 
-@import url('https://fonts.googleapis.com/css2?family=Skranji:wght@400;700&display=swap');
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const handleImageLoad = () => {
+    setImageLoaded(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsCssLoaded(true);
+    }, 2000);
+  }, []);
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [promoVisible, setpromoVisible] = useState(true);
 
 
 
-.favicon { 
-  width: 10px;
-  height: 5px;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const { data: session } = useSession()
+
+
+  const promoCloseButtonClick = () => {
+    setpromoVisible(false); // Set isVisible to false to hide the div
+  };
+
+
+
+  return (
+    <>
+
+      <Head>
+        <title > Astrum Stellar</title>
+
+        <link className={styles.favicon} height={100} width={100} rel="icon" href="/logoblack.ico" type="image/x-icon" />
+        <link rel="stylesheet" href={"/styles/components/header.module.css"} />
+        <link rel="stylesheet" href={"/styles/components/header_xsm_mobile.module.css"} />
+      </Head>
+
+      {!isCssLoaded && <Loading />} {/* Show loading screen while CSS is not loaded */}
+      {isCssLoaded && (
+
+        <div className={`${styles.parent} ${xsmStyles.parent_xsm}`}>
+          <div className={`${styles.parent_wrapper} ${xsmStyles.parent_wrapper_xsm} ${isVisible ? '' : styles.parent_wrapper_margin}`}>
+            <ol className={`${styles.logo__signature_and_button_parent} ${xsmStyles.logo__signature_and_button_parent_xsm}`}>
+
+
+              <ul className={`${styles.logo_and_signature} ${xsmStyles.logo_and_signature_xsm}`}>
+                <li className={`${styles.logo_item} ${xsmStyles.logo_item_xsm}`}>
+                  <Link
+                    href={"/"}>
+                    <Image
+                      className={`${styles.header_logo} ${xsmStyles.header_logo_xsm}`}
+                      alt=""
+                      width={50}
+                      height={50}
+                      src={"/logored.png"}
+                    >
+                    </Image>
+                  </Link>
+                </li>
+
+                <li className={styles.signature_item}>
+                  <Link href={"/"}>
+                    <Image
+                      className={`${styles.header_signature} ${xsmStyles.header_signature_xsm}`} aria-label="signature"
+                      alt=""
+                      width={200}
+                      height={200}
+                      src={"/signaturewhite.png"}
+                    >
+                    </Image>
+                  </Link>
+                </li>
+              </ul>
+
+
+
+              <ol className={`${styles.buttons} ${xsmStyles.buttons_xsm}`}>
+                <ul className={styles.login_logout_list}>
+                  {session ? (
+                    <>
+
+                      <li className={styles.register_button_item}>
+                        <button className={`${styles.logout_button_item} ${xsmStyles.register_button_xsm}`} onClick={() => signOut('github')()}>
+                          <p>Logout</p>
+                        </button>
+                      </li>
+                    </>
+                  ) : (
+                    <>
+                      <li className={styles.logout_button_item}>
+                        <button className={`${styles.login_button} ${xsmStyles.login_button_xsm}`} onClick={() => signIn('google')}>
+                          <p>Google signIn</p>
+                        </button>
+                      </li>
+                      <li className={styles.logout_button_item}>
+                        <button className={`${styles.login_button} ${xsmStyles.login_button_xsm}`} onClick={() => signIn('github')}>
+                          <p>GitHub signIn</p>
+                        </button>
+                      </li>
+                    </>
+                  )}
+                </ul>
+              </ol>
+            </ol>
+            <ol className={styles.menu_bars_wrapper}>
+              <ul className={`${styles.first_menubar} ${xsmStyles.first_menubar_xsm}`} style={{ display: isVisible ? 'inline-flex' : 'none' }}>
+                <li className={styles.first_menubar_item}>
+                  <Link href={'/info/privatedomains'}>
+                    <p className={styles.promo1}>
+                      Private Domains?
+                    </p>
+                  </Link>
+                </li>
+                <li className={styles.first_menubar_item}>
+                  <Link href={'/info/applicationinterfaces'}>
+                    <p className={styles.promo2}>
+                      Application Interfaces?
+                    </p>
+                  </Link>
+                </li>
+                <li className={styles.first_menubar_item}>
+                  <Link href={'/info/threedrenders'}>
+                    <p className={styles.promo3}>
+                      3D Renders?
+                    </p>
+                  </Link>
+                </li>
+                <li className={styles.first_menubar_item}>
+                  <Link href={'/info/vercel'}>
+                    <p className={styles.promo4}>
+                      Vercel?
+                    </p>
+                  </Link>
+                </li>
+                <li className={styles.first_menubar_item}>
+                  <Link href={'/info/hostgatorvercel'}>
+                    <p className={styles.promo4}>
+                      Hostgator/Bluehost?
+                    </p>
+                  </Link>
+                </li>
+
+              </ul>
+
+
+              <ul className={`${styles.second_menubar} ${xsmStyles.second_menubar_xsm}`}>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="/"
+                  >
+                    <p className={styles.second_menubar_item}>Home</p>
+                  </Link >
+                </li>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="/services"
+                  >
+                    <p className={styles.second_menubar_item}>Services</p>
+                  </Link>
+                </li>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="/pricing"
+                  >
+                    <p className={styles.second_menubar_item}>Pricing</p>
+                  </Link>
+                </li>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="/projects"
+                  >
+                    <p className={styles.second_menubar_item}>Projects</p>
+                  </Link>
+                </li>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="/contact"
+                  >
+                    <p className={styles.second_menubar_item}>Contact Me</p>
+                  </Link>
+                </li>
+
+                <li className={styles.menuitem}>
+                  <Link legacyBehavior
+                    href="https://github.com/deodagee?tab=repositories"
+                  >
+                    <p className={styles.second_menubar_item}>Source Code</p>
+                  </Link>
+                </li>
+
+              </ul>
+
+              {session ? (
+                <>
+                  <div className={`${styles.promo_message_first} ${xsmStyles.promo_message_first_xsm}`} style={{ display: promoVisible ? 'inline-flex' : 'none' }}>
+                    <p>Congratulations! You&apos;ve earned a higher rebate for signing in. Mention promo code: <span className={styles.promo_code}>&quot;STELLAR19950202&quot; </span> when you send your request to get 70% off</p>
+                    <button className={styles.close_button} onClick={promoCloseButtonClick}>
+                      <div>
+                        <Image
+                          className={styles.close_button_image}
+                          alt="closebutton"
+                          width={100}
+                          height={100}
+                          src={"/closebutton.png"}
+                        >
+                        </Image>
+                      </div>
+                      <div>
+                        <p>
+                          Close
+                        </p>
+                      </div>
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div className={styles.nothing}>
+                </div>
+              )}
+              <div className={`${styles.promo_message_first} ${xsmStyles.promo_message_first_xsm}`} style={{ display: promoVisible ? 'inline-flex' : 'none' }}>
+                <h1>Thank you for visiting the site. I am currently renovating and changing things up so please excuse the frequently changing Ui. If you&apos;re ready to purchase, you can absolutely get 60% off everything I&apos;ve priced on the site. Log in to take part of the 70% promotions. This is only for a limited time, I will change prices back once enough clients have taken into the promotion.</h1>
+                <button className={styles.close_button} onClick={promoCloseButtonClick}><div>
+                        <Image
+                          className={styles.close_button_image}
+                          alt="closebutton"
+                          width={100}
+                          height={100}
+                          src={"/closebutton.png"}
+                        >
+                        </Image>
+                      </div>
+                      <div>
+                        <p>
+                          Close
+                        </p>
+                      </div>
+                    </button>
+              </div>
+            </ol>
+
+
+          </div>
+        </div>
+      )}
+    </>
+  )
+
 }
 
-
-
-.parent ul {
-  list-style: none;
-}
-
-.parent {
-  width: 100vw;
-  z-index: 100;
-  color: white;
-  display: flex;
-  overflow: hidden;
-  position: absolute;
-}
-
-@keyframes textColorCycle_name {
-
-  0%,
-  100% {
-    background-color: #101010;
-  }
-
-  33% {
-    background-color: #fafafafb;
-  }
-
-  66% {
-    background-color: #131313;
-  }
-}
-
-.parent_wrapper {
-  position: fixed;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  transition: 0.6s;
-}
-
-.parent_wrapper_margin {
-  transition: 0.6s;
-}
-
-.logo__signature_and_button_parent {
-  background: linear-gradient(to top, #0e0e0e, #1e1e1e, #202020, #323232);
-  display: flex;
-  width: 100vw;
-  justify-content: space-between;
-  flex-direction: row;
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
-
-.logo_and_signature {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: max-content;
-  margin-top: 5px;
-}
-
-.logo_item {
-  margin: 5px;
-  margin-top: 0px;
-  margin-left: 20px;
-}
-
-.header_logo {
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  box-shadow: white 0px 0px 7px;
-  transition: 0.6s;
-}
-
-.header_logo:hover {
-  transition: 0.6s;
-  transform: scale(0.9);
-  cursor: pointer;
-  margin-top: 0;
-  box-shadow: white 0px 0px 12px;
-}
-
-.signature_item {
-  margin: 5px;
-}
-
-.header_signature {
-  margin-left: 10vw;
-  margin-top: 6px;
-  transition: 0.5s;
-  width: 100px;
-  height: 30px;
-}
-
-.header_signature:hover {
-  cursor: pointer;
-  transition: 0.5s;
-  transform: scale(1.1);
-}
-
-.hide {
-  display: none;
-}
-
-.menu_bars_wrapper {
-  gap: 1px;
-  display: flex;
-  flex-direction: column;
-  margin-top: 0px;
-  align-items: center;
-  justify-content: center;
-}
-
-.menuitem { 
-  cursor: pointer;
-}
-
-.first_menubar {
-  gap: 10vw;
-  width: 100vw;
-  display: flex;
-  font-size: 9px;
-  font-family: 'Skranji', cursive;
-  font-weight: 700;
-  flex-direction: row;
-  white-space: nowrap;
-  align-items: center;
-  justify-content: center;
-  background-color: rgb(34, 34, 34);
-  background: linear-gradient(to top, #111316, #11141a);
-  padding: 5px;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  color: rgb(255, 255, 255);
-  position: fixed;
-  text-decoration: underline;
-  transition: 0.8s;
-}
-
-.first_menubar p:hover {
-  cursor: pointer;
-  transition: 0.3s;
-  color: rgb(0, 68, 93);
-}
-
-.first_menubar:hover {
-  transition: 0.5s;
-  color:#f0f0f0;
-  background: linear-gradient(to top, #171717, #191919);
-    box-shadow: rgb(255, 255, 255) 0px 0px 5px;
-}
-
-.second_menubar {
-  gap: 12vw;
-  width: 100vw;
-  display: flex;
-  font-size: 10px;
-  font-family: 'Skranji', cursive;
-  font-weight: 700;
-  flex-direction: row;
-  white-space: nowrap;
-  align-items: center;
-  justify-content: center;
-  background-color: rgb(17, 17, 17);
-  padding: 5px;
-  padding-top: 8px;
-  padding-bottom: 8px;
-  border-top: none;
-  border-left: none;
-  border-right: none;
-  border-color: rgb(212, 212, 212);
-  color: rgb(246, 246, 246);
-  border-width: 1px;
-  box-shadow: rgb(255, 255, 255) 0px 0px 3px;
-  transition: 0.5s;
-}
-
-.second_menubar:hover {
-  color: rgb(255, 255, 255);
-  background-color: rgb(31, 31, 31);
-  transition: 0.5s;
-  box-shadow: rgb(255, 255, 255) 0px 0px 5px;
-}
-
-.second_menubar p {
-  transition: 0.7s;
-}
-
-.second_menubar p:hover {
-  cursor: pointer;
-  color: rgb(250, 250, 250);
-  transition: 0.4s;
-  text-decoration: underline;
-}
-
-.buttons {
-  margin-top: 10px;
-  display: flex;
-  margin-right: 2vw;
-  font-size: 15px;
-  font-family: 'Teko', sans-serif;
-  flex-direction: row;
-  margin-left: auto;
-  font-weight: 600;
-  color: rgb(255, 255, 255);
-}
-
-.login_logout_list { 
-  display: flex;
-  flex-direction: row;
-}
-
-.promo_message_first{
-  position: absolute; 
-  margin-top: 80vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(194, 194, 194, 0.699);
-  border-width: 1px;
-  border-color: orange;
-  width: 90vw;
-  height: 250px;
-  padding: 10px;
-  font-weight: 400;
-  font-size: 11px;
-  text-align: center;
-font-family: Verdana, Geneva, Tahoma, sans-serif;
-}
-
-.promo_message_first h1{ 
-
-  text-align: left;
-  background-color: rgba(167, 114, 0, 0.762);
-  font-family: Verdana, Geneva, Tahoma, sans-serif;
-  padding: 10px;
-  border-radius: 0.45rem;
-  color: rgb(245, 245, 245);
-}
-
-.close_button  {
-  display: flex;
-  flex-direction: row;
-  position: absolute;
-  align-items: center;
-  gap: 5px;
-  justify-content: center;
-  margin-top: 200px;
-  margin-left: 20px;
-  background-color: black;
-  border-radius: 0.15rem;
-  box-shadow: white 0px 0px 4px;
-  padding: 10px;
-  transition: 0.5s;
-}
-.close_button_image{ 
-  width: 10px;
-  height: 10px;
-}
-
-.close_button:hover { 
-  background-color: black;
-  transition: 0.5s;
-  transform: scale(1.05);
-}
-
-.promo_code { 
-  color: rgb(237, 197, 52);
-}
-
-.register_button {
-  color: rgb(40, 40, 40);
-  white-space: nowrap;
-  margin-left: 10px;
-  margin-bottom: 5px;
-  border-width: 0.5px;
-  border-color: white;
-  width: max-content;
-  padding: 3px;
-  padding-left: 20px;
-  padding-right: 20px;
-  background: linear-gradient(to top, #ededed, #efefef, #f7f7f7f1);
-  border-radius: 0.99rem;
-  transition: 0.6s;
-}
-
-.register_button:hover {
-  color: rgb(224, 224, 224);
-  background: linear-gradient(to top, #0a0a0a, #0e0e0e, #232323f1);
-  transition: 0.6s;
-  box-shadow: white 0px 0px 5px;
-}
-
-.login_button {
-  color: rgb(40, 40, 40);
-  white-space: nowrap;
-  margin-left: 10px;
-  margin-bottom: 5px;
-  border-width: 0.5px;
-  border-color: rgb(255, 174, 0);
-  width: max-content;
-  padding: 3px;
-  padding-left: 20px;
-  padding-right: 20px;
-  padding-top: 4px;
-  background: linear-gradient(to top, #ededed, #efefef, #f7f7f7f1);
-  border-radius: 0.25rem;
-  transition: 0.6s;
-}
-
-.login_button:hover {
-  color: rgb(227, 227, 227);
-  background: linear-gradient(to top, #0a0a0a, #0e0e0e, #232323f1);
-  transition: 0.6s;
-  box-shadow: white 0px 0px 5px;
-}
-
+export default Header;
