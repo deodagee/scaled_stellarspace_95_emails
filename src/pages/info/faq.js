ee12e705cbac06280714b1e4c6290bb622c160fc
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-11 01:03:43 -0400

    Add files via upload

---
+++ src/pages/info/faq.js
@@ -0,0 +1,18 @@
+import Header from '../components/header'
+import FaqPage from "../components/info/faq.js"
+import Footer from "../components/footer"
+
+function FaqMain() {
+    return (
+        <>
+            <div>
+                <Header></Header>
+                <FaqPage></FaqPage>
+                <Footer></Footer>
+            </div>
+        </>
+    )
+}
+
+export default FaqMain;
+
