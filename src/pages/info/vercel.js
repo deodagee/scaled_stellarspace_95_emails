872de46f0104046ffc761cb48080e9cb055d0fbf
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-12 08:45:17 -0400

    Update vercel.js

--- src/pages/info/vercel.js
+++ src/pages/info/vercel.js
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google'
 import Header from '../components/header'
 import VercelPage from "../components/info/vercel.js"
 import Footer from "../components/footer"
-import styles from "../../styles/components/info/vercel.module.css"
+import styles from "../../styles/components/info/faq.module.css"
 const inter = Inter({ subsets: ['latin'] })
 
 function threedrenders() {
