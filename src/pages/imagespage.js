aae4efb03fe639f2b74516e67021ff204ed5ba99
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-10-16 03:04:46 -0400

    Add files via upload

---
+++ src/pages/imagespage.js
@@ -0,0 +1,20 @@
+import Header from './components/header'
+import ImagesPage from "./components/imagespage"
+import Footer from "./components/footer"
+
+
+function Images() {
+  return (
+    <>
+
+      <Header></Header>
+
+      <ImagesPage></ImagesPage>
+      <Footer></Footer>
+
+    </>
+  )
+}
+
+export default Images;
+
