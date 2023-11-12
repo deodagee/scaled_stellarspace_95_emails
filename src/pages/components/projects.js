900b5d13c896543e98dce0f536a284695f5a5ffb
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-11-12 13:57:00 -0500

    Update projects.js

--- src/pages/components/projects.js
+++ src/pages/components/projects.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import styles from "../../styles/components/projects.module.css"
 import Link from "next/link";
 import { useState } from "react";
