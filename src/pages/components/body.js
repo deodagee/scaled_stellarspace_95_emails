70208dea26c31352cb0b46b7bdb8330d3aea04b1
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-11-12 14:53:04 -0500

    Update body.js

--- src/pages/components/body.js
+++ src/pages/components/body.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import  { useEffect, useRef } from 'react';
 import Image from "next/image";
 import styles from "../../styles/components/body.module.css";
 import Link from "next/link";
