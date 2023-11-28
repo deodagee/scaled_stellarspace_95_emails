2c7a9716b9a779bc981a1baf36c272ddb2d7a166
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-11-28 05:26:13 -0500

    Update willcalls.js

--- src/pages/info/willcalls.js
+++ src/pages/info/willcalls.js
@@ -1,157 +1,31 @@
-import React from "react";
-import Link from "next/link";
-import styles from "../../../styles/components/info/willcalls.module.css"
-import Image from "next/image";
-import { useEffect, useRef, useState } from "react";
+import { Inter } from 'next/font/google'
+import Header from '../components/header'
+import WillCalls from "../components/info/willcalls.js"
+import Footer from "../components/footer"
+import styles from "../../styles/components/info/willcalls.module.css"
+const inter = Inter({ subsets: ['latin'] })
 
-
-
-
-function WillCalls () {
-
-    const [videoLoaded, setvideoLoaded] = useState(false);
-    const handleVideoLoad = () => {
-        setvideoLoaded(true);
-    };
-
-    const videoRef = useRef(null);
-
-    useEffect(() => {
-        const video = videoRef.current;
-
-        if (video) {
-            video.play().catch(error => {
-                // Handle autoplay error (e.g., user interaction required)
-                console.error('Autoplay error:', error);
-            });
-        }
-    }, []);
-
-    useEffect(() => {
-        document.body.style.overflowX = 'hidden';
-    
-        return () => {
-          document.body.style.overflowX = ''; 
-        };
-      }, []);
-
-    return (
-        <>
-            <div className={styles.projects_page_whole}>
-                <div className={styles.projects_page_wrapper}>
-
-                    <div className={styles.list_div}>
-                        <ol className={styles.title_and_title_description}>
-                            <Image
-                                className={styles.logored}
-                                alt=""
-                                width={100}
-                                height={100}
-                                src={"/logored.png"}></Image>
-                            <li className={styles.title1}><p><h1>Product Modeling<span className={styles.title_brother}> Logo Designing </span></h1></p></li>
-                            <li className={styles.description_list_element}>
-                                <p>
-                                    Details
-                                </p>
-                            </li>
-
-                            <li className={styles.buttons_list_element}>
-                                <button className={styles.contact_button}><Link href={"/contact"}>Contact Page</Link></button>
-                                <button className={styles.see_more_button}><Link href={"/"}>Home Page</Link></button>
-                            </li>
-                        </ol>
-                    </div>
-
-                    <div className={styles.categories_wrapper}>
-                        <ol className={styles.list2}>
-                            <li className={styles.titles_group}>
-                                <span>
-                                    <Image
-                                        className={styles.chevron}
-                                        alt="chevron"
-                                        src={'/chevron.png'}
-                                        width={100}
-                                        height={100}
-                                    ></Image>
-                                    <h1>Info</h1>
-                                </span>
-                                <span>
-                                    <Image
-                                        className={styles.chevron}
-                                        alt="chevron"
-                                        src={'/chevron.png'}
-                                        width={100}
-                                        height={100}
-                                    ></Image>
-                                    <h2>Photoshop & Illustrator</h2>
-                                </span>
-                                <span>
-                                    <Image
-                                        className={styles.chevron}
-                                        alt="chevron"
-                                        src={'/chevron.png'}
-                                        width={100}
-                                        height={100}
-                                    ></Image>
-                                    <p>Artist</p>
-                                </span>
-
-                            </li>
-                            <li className={styles.title}>
-                                <h1>
-                                Original Designs | Copyright 
-                                </h1>
-                                <Image
-                                    className={styles.mid_page_image}
-                                    alt="willcalls"
-                                    height={1000}
-                                    width={1000}
-                                    src={"/willcalls2.jpg"}>
-
-                                </Image>
-                            </li>
-                        </ol>
-                        
-                    </div>
-
-
-
-
-                    <div>
-                        <ol className={styles.blog_part}>
-                            <li className={styles.blog_part_title}>
-                                <h1>Site Info</h1>
-                            </li>
-                            <li className={styles.blog_part_title2}>
-                                <h2>Details</h2>
-                            </li>
-                            <li className={styles.blog_part_paragraph}>
-                                <p>Logo Designing | Branding</p>
-                            </li>
-                        </ol>
-                        <ol className={styles.blog_part}>
-                            <li className={styles.blog_part_paragraph}>
-                                <p>2023-10-21</p>
-                            </li>
-                            <li className={styles.blog_part_paragraph}>
-                                <p>Project<span className={styles.darkened}>closed</span></p>
-                            </li>
-                            
-                        </ol>
-                        <ol className={styles.blog_part}>
-                            <li className={styles.blog_part_title}>
-                                <h1>StellarSpace | Astrum Stellar</h1>
-                            </li>
-                            <li className={styles.blog_part_paragraph}>
-                                <p></p>
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-            </div>
-
-        </>
-    )
+function WillCallsPage() {
+  return (
+    <>
+    <div className={styles.front_end_apis_page}>
+    <div className={styles.front_end_apis_page_wrapper}>
+    <div className={styles.front_end_apis_page_header}>
+      <Header></Header>
+      </div>
+      <div className={styles.front_end_apis_page_body}>
+      <WillCalls></WillCalls>
+      </div>
+      <div className={styles.front_end_apis_page_footer}>
+      <Footer></Footer>
+      </div>
+      </div>
+      </div>
+    </>
+  )
 }
 
-export default WillCalls;
+export default WillCallsPage;
+
+
+
