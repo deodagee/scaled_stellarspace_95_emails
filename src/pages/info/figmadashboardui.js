15e352e04f9777a38e951feacdd09aa7f3f06648
Astrum Stellar <124405934+deodagee@users.noreply.github.com>
2023-12-08 19:49:28 -0500

    Add files via upload

---
+++ src/pages/info/figmadashboardui.js
@@ -0,0 +1,31 @@
+import { Inter } from 'next/font/google'
+import Header from '../components/header'
+import Footer from "../components/footer"
+import styles from "../../styles/components/info/figmadashboardui.module.css"
+import FigmaDashboardUi from '../components/info/figmadashboardui'
+const inter = Inter({ subsets: ['latin'] })
+
+function FigmaDashboardUiPage() {
+  return (
+    <>
+    <div className={styles.front_end_apis_page}>
+    <div className={styles.front_end_apis_page_wrapper}>
+    <div className={styles.front_end_apis_page_header}>
+      <Header></Header>
+      </div>
+      <div className={styles.front_end_apis_page_body}>
+      <FigmaDashboardUi></FigmaDashboardUi>
+      </div>
+      <div className={styles.front_end_apis_page_footer}>
+      <Footer></Footer>
+      </div>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default FigmaDashboardUiPage;
+
+
+
